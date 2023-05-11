7697dde9ec801b1977d25564f0242bfb2451eacc
HieuNghia0000 <fakenamemc1@gmail.com>
2023-05-11 22:46:04 +0700

    Components Part 2

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer>
+                <p>Copyright 2023</p>
+            </footer>
+        )
+    }
+}
